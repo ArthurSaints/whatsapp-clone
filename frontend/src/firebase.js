c8bbda46897118eb9c6a4169a3ab91cd174179f9
Arthur Santos <arthursantosgc@gmail.com>
2020-09-08 17:16:49 -0300

    limpando os arquivos do projeto

---
+++ frontend/src/firebase.js
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyBVuvL0mQn7sZYZOy12owl_lQsb_BFPhV8",
+    authDomain: "whatsapp-clone-5d50c.firebaseapp.com",
+    databaseURL: "https://whatsapp-clone-5d50c.firebaseio.com",
+    projectId: "whatsapp-clone-5d50c",
+    storageBucket: "whatsapp-clone-5d50c.appspot.com",
+    messagingSenderId: "793341016788",
+    appId: "1:793341016788:web:4052f859b23cfa8d8ed941"
+};
+
+export default firebaseConfig;
