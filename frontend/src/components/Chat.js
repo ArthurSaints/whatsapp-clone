fd2762393a39347e79dd755da87a9e67a4718fc0
Arthur Santos <arthursantosgc@gmail.com>
2020-09-10 22:48:59 -0300

    chat body finalizado

--- frontend/src/components/Chat.js
+++ frontend/src/components/Chat.js
@@ -1,11 +1,52 @@
 import React from "react";
 
+import AttachFileIcon from "@material-ui/icons/AttachFile";
+import MoreVertIcon from "@material-ui/icons/MoreVert";
+import { SearchOutlined } from "@material-ui/icons";
+import { Avatar, IconButton } from "@material-ui/core";
+
 import "./Chat.css";
 
 export default function Chat() {
   return (
     <div className="chat">
-      <h1>i'm a chat</h1>
+      <div className="chat__header">
+        <Avatar />
+
+        <div className="chat__headerInfo">
+          <h3>Room name</h3>
+          <p>Last seen at...</p>
+        </div>
+
+        <div className="chat__headerRight">
+          <IconButton>
+            <SearchOutlined />
+          </IconButton>
+          <IconButton>
+            <AttachFileIcon />
+          </IconButton>
+          <IconButton>
+            <MoreVertIcon />
+          </IconButton>
+        </div>
+      </div>
+
+      <div className="chat__body">
+        <p className="chat__message">
+          <span className="chat__name">Arthur S.</span>
+          This is a message
+          <span className="chat__timestamp">
+            {new Date().toLocaleTimeString()}
+          </span>
+        </p>
+        <p className="chat__message chat__reciever">
+          <span className="chat__name">Arthur S.</span>
+          This is a message
+          <span className="chat__timestamp">
+            {new Date().toLocaleTimeString()}
+          </span>
+        </p>
+      </div>
     </div>
   );
 }
