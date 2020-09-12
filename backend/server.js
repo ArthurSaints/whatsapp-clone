a44031867f04466972552f21f86ab45d7db05342
Arthur Santos <arthursantosgc@gmail.com>
2020-09-11 21:41:25 -0300

    backend finalizado

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,7 @@
 // importing
 import express from "express";
 import mongoose from "mongoose";
-import pusher from "pusher";
+import Pusher from "pusher";
 
 import Messages from "./dbMessages.js";
 
@@ -10,51 +10,76 @@ const app = express();
 const port = process.env.PORT || 9000;
 
 const pusher = new Pusher({
-  appId: "1071549",
-  key: "951b6cbb760c52ab3a97",
-  secret: "a45e44dda7a17a3fade1",
-  cluster: "us2",
-  encrypted: true,
+    appId: "1071549",
+    key: "951b6cbb760c52ab3a97",
+    secret: "a45e44dda7a17a3fade1",
+    cluster: "us2",
+    encrypted: true,
 });
 
 // middlewares
 app.use(express.json());
 
-// DB Config
-const connectionURL =
-  "mongodb+srv://admin:kA7bFrPqgugOUwvM@cluster0.yitjo.mongodb.net/whatsapp-clone-db?retryWrites=true&w=majority";
-
-mongoose.connect(connectionURL, {
-  useCreateIndex: true,
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
+app.use((req, res, next) => {
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.setHeader("Access-Control-Allow-Headers", "*");
+    next();
 });
 
-// ???
+// DB Config
+const connectionURL =
+    "mongodb+srv://admin:kA7bFrPqgugOUwvM@cluster0.yitjo.mongodb.net/whatsapp-clone-db?retryWrites=true&w=majority";
+
+mongoose.connect(connectionURL, {
+    useCreateIndex: true,
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+});
+
+// triggers
+const db = mongoose.connection;
+db.once("open", () => {
+    console.log("DB connected");
+
+    const msgCollection = db.collection("messagecontents");
+    const changeStream = msgCollection.watch();
+
+    changeStream.on("change", (change) => {
+        if (change.operationType === "insert") {
+            const messageDetails = change.fullDocument;
+            pusher.trigger("messsages", "inserted", {
+                name: messageDetails.name,
+                message: messageDetails.message,
+            });
+        } else {
+            console.log("Error triggering Pusher");
+        }
+    });
+});
 
 // api routes
 app.get("/", (req, res) => res.status(200).send("Hello world!"));
 
 app.get("/messages/sync", (req, res) => {
-  Messages.find((err, data) => {
-    if (err) {
-      res.status(500).send(err);
-    } else {
-      res.status(201).send(data);
-    }
-  });
+    Messages.find((err, data) => {
+        if (err) {
+            res.status(500).send(err);
+        } else {
+            res.status(201).send(data);
+        }
+    });
 });
 
 app.post("/messages/new", (req, res) => {
-  const dbMessaage = req.body;
+    const dbMessaage = req.body;
 
-  Messages.create(dbMessaage, (err, data) => {
-    if (err) {
-      res.status(500).send(err);
-    } else {
-      res.status(201).send(`New message created: \n ${data}`);
-    }
-  });
+    Messages.create(dbMessaage, (err, data) => {
+        if (err) {
+            res.status(500).send(err);
+        } else {
+            res.status(201).send(`New message created: \n ${data}`);
+        }
+    });
 });
 
 // listen
